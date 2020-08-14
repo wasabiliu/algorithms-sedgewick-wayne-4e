5d32f2275a1726cd86dc8103a37a9b7c7ea387a8
valineliu <valineliu@tencent.com>
2020-08-14 20:19:55 +0800

    add chapter 1 section 1 exercise 1

---
+++ src/chapter1/Exercise01_GiveValues.java
@@ -0,0 +1,13 @@
+package ch1.section1;
+
+/**
+ * 1.1.1
+ * Give the value of each of the following expressions.
+ */
+public class Exercise01_GiveValues {
+    public static void main(String[] args) {
+        System.out.println((0 + 15) / 2);                   // 7
+        System.out.println(2.0e-6 * 100000000.1);           // 200.0000002
+        System.out.println(true && false || true && true);  // true
+    }
+}
