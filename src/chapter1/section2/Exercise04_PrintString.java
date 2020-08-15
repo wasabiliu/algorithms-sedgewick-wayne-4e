299e328b55809d3dad6cfc1660ba3c75369bd949
wasabiliu <wasabiliu@hotmail.com>
2020-08-15 15:03:29 +0800

    change exercise 04 name

--- src/chapter1/section2/Exercise04_PrintString.java
+++ src/chapter1/section2/Exercise04_PrintString.java
@@ -2,7 +2,7 @@
  * 1.2.4
  * What does the following code fragment print?
  */
-public class Exercise03_PrintString {
+public class Exercise04_PrintString {
     public static void printString() {
         String string1 = "hello";
         String string2 = string1;
