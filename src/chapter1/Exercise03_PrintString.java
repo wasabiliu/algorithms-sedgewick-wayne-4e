e1544dfc960216259c4db1117b98abacf0f9b8bb
wasabiliu <wasabiliu@hotmail.com>
2020-08-15 15:01:59 +0800

    add exercise 04

---
+++ src/chapter1/Exercise03_PrintString.java
@@ -0,0 +1,17 @@
+/**
+ * 1.2.4
+ * What does the following code fragment print?
+ */
+public class Exercise03_PrintString {
+    public static void printString() {
+        String string1 = "hello";
+        String string2 = string1;
+        string1 = "world";
+        System.out.println(string1);    // world
+        System.out.println(string2);    // hello
+    }
+
+    public static void main(String[] args) {
+        printString();
+    }
+}
