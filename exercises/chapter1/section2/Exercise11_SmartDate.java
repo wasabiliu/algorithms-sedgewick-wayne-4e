a65ccc9a98e55f81d4f8f575870d02147977c92c
wasabiliu <wasabiliu@hotmail.com>
2020-08-15 22:13:18 +0800

    add exercise 11

---
+++ exercises/chapter1/section2/Exercise11_SmartDate.java
@@ -0,0 +1,54 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+/**
+ * 1.2.11
+ * Develop an implementation SmartDate of our Date API that raises an exception
+ * if the date is not legal.
+ */
+public class Exercise11_SmartDate {
+    static class SmartDate {
+        private int year;
+        private int month;
+        private int day;
+
+        public SmartDate(int year, int month, int day) throws IllegalArgumentException {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            if (!isValid()) throw new IllegalArgumentException();
+        }
+
+        public int year() {
+            return year;
+        }
+
+        public int month() {
+            return month;
+        }
+
+        public int day() {
+            return day;
+        }
+
+        private boolean isValid() {
+            try {
+                DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+                df.setLenient(false);
+                df.parse(this.toString());
+            } catch (Exception e) {
+                return false;
+            }
+            return true;
+        }
+
+        public String toString() {
+            return month + "/" + day + "/" + year;
+        }
+    }
+
+    public static void main(String[] args) {
+        SmartDate d = new SmartDate(1999, 12, 31);
+        System.out.println(d);
+    }
+}
